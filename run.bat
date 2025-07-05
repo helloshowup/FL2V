4b1dc8b41c69ed0f20766e765022b0875c6bc4a8
helloshowup <132871062+helloshowup@users.noreply.github.com>
2025-07-05 19:48:04 +0200

    Add error handling to run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,12 @@
 @echo off
-python -m venv venv
-call venv\Scripts\activate
-pip install -r requirements.txt
-python main.py
+
+python -m venv venv || goto :error
+call venv\Scripts\activate || goto :error
+pip install -r requirements.txt || goto :error
+python main.py || goto :error
+
+exit /b 0
+
+:error
+echo launch terminal > error.log
+exit /b 1
